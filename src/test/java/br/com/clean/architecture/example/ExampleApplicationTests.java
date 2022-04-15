e29c387f517a57ff08cc9c14d23bcff2634e3841
Michel Medeiros <michel.tds@gmail.com>
2022-04-15 10:04:46 -0300

    First commit

---
+++ src/test/java/br/com/clean/architecture/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.clean.architecture.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

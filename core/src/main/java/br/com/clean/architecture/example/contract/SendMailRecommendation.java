323ecf5993a6f38677013e336e371567d1c697d5
Michel Medeiros <michel.tds@gmail.com>
2022-04-18 14:16:49 -0300

    Replace files

--- core/src/main/java/br/com/clean/architecture/example/contract/SendMailRecommendation.java
+++
@@ -1,8 +0,0 @@
-package br.com.clean.architecture.example.contract;
-
-
-import br.com.clean.architecture.example.entity.Student;
-
-public interface SendMailRecommendation {
-    void sendMail(Student student);
-}
